7782a322135b05f37c868862f0b78fb9e3b3f880
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2025-07-13 22:57:46 +0700

    feat: add nest g resource accounts, auth, transactions, users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsModule } from './accounts/accounts.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AccountsModule, AuthModule, TransactionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
